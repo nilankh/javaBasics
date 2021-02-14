33ba0b72009e23cf219e8b5e70a6b1b3757b4338
nilank nikhil <nilanknikhil@gmail.com>
2021-02-14 19:00:31 +0530

    learning encapuslation

--- encapsulation/EncapsulationInfo.java
+++ encapsulation/EncapsulationInfo.java
@@ -3,8 +3,12 @@ package encapsulation;
 public class EncapsulationInfo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public void doWork() {
+		System.out.println("Working working");
 	}
 
 }
