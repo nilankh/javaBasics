c1c199020d3ded43ec3e34d8ef9fe018c8733adc
nilank nikhil <nilanknikhil@gmail.com>
2021-02-14 19:13:00 +0530

    learning encapuslation

--- encapsulation/EncapsulationInfo.java
+++ encapsulation/EncapsulationInfo.java
@@ -3,7 +3,9 @@ package encapsulation;
 public class EncapsulationInfo {
 
 	public static void main(String[] args) {
-		
+		Laptop l1 = new Laptop();
+//		l1.price = 23;
+		l1.setPrice(31);
 
 	}
 	
@@ -18,8 +20,23 @@ public class EncapsulationInfo {
 //	}
 	
 //	private
-	private void doWork() {
-		System.out.println("Working Working");
+//	private void doWork() {
+//		System.out.println("Working Working");
+//	}
+	
+}
+class Laptop{
+	int ram;
+	private int price;
+	
+	public void setPrice(int price) {
+//		is the user an Admin
+		boolean isAdmin = false;
+		if(!isAdmin) {
+			System.out.println("You can not set the price.");
+		}else {
+			this.price = price;
+		}
+		
 	}
-
 }
